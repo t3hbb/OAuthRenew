bce8adae9b2b3249c9f0527da89cb418a80e3dc0
t3hbb <dispacct@hotmail.com>
2020-07-03 08:37:27 +0100

    Update OAuthRenew.py

--- OAuthRenew.py
+++ OAuthRenew.py
@@ -24,8 +24,6 @@ AccessTokenRegex = re.compile(r"access_token\"\:\"(.*?)\"")
 #Regex to identify if bearer token expired
 BearerErrorRegex = re.compile(r"Unauthorized")
 
-   
-  
 class BurpExtender(IBurpExtender, IHttpListener, ISessionHandlingAction):
 	# Variables to hold the tokens found so that it can be inserted in the next request
 	discoveredBearerToken = ''
@@ -34,7 +32,7 @@ class BurpExtender(IBurpExtender, IHttpListener, ISessionHandlingAction):
 	def registerExtenderCallbacks(self, callbacks):
 		 self._callbacks = callbacks
 		 self._helpers = callbacks.getHelpers()
-		 callbacks.setExtensionName("OAuthRenewalTool")
+		 callbacks.setExtensionName("OAuthRefresher")
 		 callbacks.registerHttpListener(self)
 		 print "Extension registered successfully."
 		 return
@@ -43,7 +41,9 @@ class BurpExtender(IBurpExtender, IHttpListener, ISessionHandlingAction):
 		# Operate on all tools other than the proxy. Comment out if you are passing stuff through from Postman or SoapUI
 		#if toolFlag != self._callbacks.TOOL_PROXY:
 		if messageIsRequest:
-			self.processRequest(currentMessage)
+			#Check to see if a replacement bearer token exists
+			if BurpExtender.discoveredBearerToken != '':
+				self.processRequest(currentMessage)
 		else:
 			self.processResponse(currentMessage)
  
@@ -78,38 +78,31 @@ class BurpExtender(IBurpExtender, IHttpListener, ISessionHandlingAction):
 		
 		updatedheaders = headerStr
 		
-		if BurpExtender.discoveredBearerToken != '':
 		# Update Bearer token
-			print "Replacing Bearer Token with latest obtained"#,BurpExtender.discoveredBearerToken #Uncomment first hash to see bearer token
-			updatedheaders = re.sub(r"Authorization\: .*", "Authorization: Bearer {0}".format(BurpExtender.discoveredBearerToken), headerStr)
-		#else:
-			#print "No Bearer token to replace."	
-		#Commented out to reduce output in log.
-
-		#convert headers into a list
+		print "Replacing Bearer Token with latest obtained"#,BurpExtender.discoveredBearerToken #Uncomment first hash to see bearer token
+		updatedheaders = re.sub(r"Authorization\: .*", "Authorization: Bearer {0}".format(BurpExtender.discoveredBearerToken), headerStr)
+		
+		#convert headers back into a list
 		headerslist = updatedheaders.splitlines()
 		updatedRequest = self._helpers.buildHttpMessage(headerslist, requestBody)
 		currentMessage.setRequest(updatedRequest)
 		
 		
 	def BearerRefresh(self):
-		#Debug String
 		print "Authing App - visiting URL"
-		#URL to visit to request a new token
-		host = "URL"
+		host = "REPLACE_AUTH_URL_HERE"
 		req = urllib2.Request(host)
-		#User agent
 		req.add_header('User-Agent','Mozilla/5.0') # (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0')
-		#Data to POST - feel free to change to how you need it
-		data = "grant_type=client_credentials&client_id=CLIENTID&client_secret=CLIENTSECRED&scope=SCOPE&audience=AUDIENCE"
+		#REPLACE data BELOW WITH APPROPRIATE VARIABLES
+		data = "grant_type=client_credentials&client_id=CLIENTID&client_secret=CLIENTSECRET&scope=SCOPE&audience=AUDIENCE"
 		req.add_data(data)
 		resp = urllib2.urlopen(req) #, context=ssl._create_unverified_context())
 		content = resp.read()
+		#print content
 		token = AccessTokenRegex.search(content)
+		#print token
 		BurpExtender.discoveredBearerToken=token.group(1)
-		print "New Bearer Token ", BurpExtender.discoveredBearerToken
-	
-
-		
+		if BurpExtender.discoveredBearerToken is None:
+			print "***Error aquiring token***"
 		
 		
